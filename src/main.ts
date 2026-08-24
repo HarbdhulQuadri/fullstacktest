ea101b1c945c6d83e5b27464136a518f460afdef
Harbdhulquadri <adegbijilanre@gmail.com>
2026-08-25 00:11:42 +0100

    fix: bundle swagger-ui-express static files in vercel serverless function using includeFiles

--- src/main.ts
+++ src/main.ts
@@ -57,14 +57,7 @@ async function bootstrap(): Promise<void> {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document, {
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
-    ],
-  });
+  SwaggerModule.setup('api/docs', app, document);
 
   app.enableCors({
     origin: resolveCorsOrigins(),
