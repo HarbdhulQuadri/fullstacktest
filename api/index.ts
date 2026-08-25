048084cf4cf3997a79833dc7aae6d60fc81f0d1f
Harbdhulquadri <adegbijilanre@gmail.com>
2026-08-25 01:56:47 +0100

    fix: use vercel rewrites to proxy swagger-ui static assets directly to cdnjs

--- api/index.ts
+++ api/index.ts
@@ -35,14 +35,7 @@ async function bootstrap() {
       .addBearerAuth()
       .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, document, {
-      customCssUrl:
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
-      customJs: [
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
-      ],
-    });
+    SwaggerModule.setup('api/docs', app, document);
     
     // Enable CORS for frontend communication
     app.enableCors({
